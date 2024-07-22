69562bf01f26094d1768fc18c0945b1acf424631
rkv5019 <rkv5019@gmail.com>
2024-07-22 14:45:27 -0700

    Removed unused import.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
